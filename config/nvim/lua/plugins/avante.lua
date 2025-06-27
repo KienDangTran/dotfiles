bbef5d8de31b760efe43c27f232fd986d47b76d9
Kien Dang <14361196+KienDangTran@users.noreply.github.com>
2025-06-27 09:58:27 +0700

    update

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -15,20 +15,16 @@ return {
   ---@module 'avante'
   ---@type avante.Config
   opts = {
-    -- add any opts here
-    -- for example
     provider = "copilot",
+    windows = {
+      width = 40,
+    },
   },
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    -- "stevearc/dressing.nvim", -- for input provider dressing
     "folke/snacks.nvim", -- for input provider snacks
     "echasnovski/mini.icons", -- or nvim-tree/nvim-web-devicons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
