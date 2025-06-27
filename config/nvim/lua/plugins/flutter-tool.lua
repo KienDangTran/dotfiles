0f9f992a7bdba35ed00aeaf8194087270a146d19
Kien Dang <kiendang@kmac.local>
2025-06-27 15:02:31 +0700

    update

--- config/nvim/lua/plugins/flutter-tool.lua
+++ config/nvim/lua/plugins/flutter-tool.lua
@@ -1,6 +1,6 @@
 return {
   "akinsho/flutter-tools.nvim",
-  lazy = false,
+  event = "VeryLazy",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim", -- optional for vim.ui.select
